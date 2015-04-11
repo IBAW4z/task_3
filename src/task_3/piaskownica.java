d94f0072ec10b482a1037b606165b37fdecc1fb7
Anna Siewierska <absinthic@gmail.com>
2015-04-11 19:58:23 +0200

    znowu poprawiłam...

--- src/task_3/piaskownica.java
+++ src/task_3/piaskownica.java
@@ -2,7 +2,7 @@ package task_3;
 
 public class piaskownica {
 	public static void main(String[] args) {
-		System.out.println("ARenc AMetz ASiewierska ASmoleñ KStrzeboñska KSkóra KSobczyk LSzetlak MPietruszewska OPu³czyñska PUszko TKowalski"); //St¹d usuwacie siebie
+		System.out.println("ARenc AMetz ASmoleñ KStrzeboñska KSkóra KSobczyk LSzetlak MPietruszewska OPu³czyñska PUszko TKowalski"); //St¹d usuwacie siebie
 		System.out.println(); //Nowa linia sposób 1
 		System.out.print("\n"); //Nowa linia sposób 2
 		System.out.println("Kto: coœ tam"); //Ka¿dy z Was dopisuje swoj¹ linijkê pod pod moj¹:
