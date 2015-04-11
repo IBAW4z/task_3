904d7120ab2cc8e6d5fc684cb688cccc81cae28c
Anna Siewierska <absinthic@gmail.com>
2015-04-11 19:55:29 +0200

    poprawiłam

--- src/task_3/piaskownica.java
+++ src/task_3/piaskownica.java
@@ -5,6 +5,7 @@ public class piaskownica {
 		System.out.println("ARenc AMetz ASiewierska ASmoleñ KStrzeboñska KSkóra KSobczyk LSzetlak MPietruszewska OPu³czyñska PUszko TKowalski"); //St¹d usuwacie siebie
 		System.out.println(); //Nowa linia sposób 1
 		System.out.print("\n"); //Nowa linia sposób 2
-		System.out.println("ASiewierska: doda³am coœ do kodu"); //Ka¿dy z Was dopisuje swoj¹ linijkê pod pod moj¹
+		System.out.println("Kto: coœ tam"); //Ka¿dy z Was dopisuje swoj¹ linijkê pod pod moj¹:
+		System.out.println("ASiewierska: doda³am coœ do kodu");
 	}
 }
