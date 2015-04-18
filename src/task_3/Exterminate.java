5b26c9a1404b99a1931596a6c711df3746b6d082
Anna Siewierska <absinthic@gmail.com>
2015-04-18 11:51:52 +0200

    extermination2

--- src/task_3/Exterminate.java
+++ src/task_3/Exterminate.java
@@ -1,4 +1,5 @@
 package task_3;
+import java.util.Scanner;
 
 public class Exterminate {
 
@@ -6,6 +7,27 @@ public class Exterminate {
 		String owner="Andziorek";
 		System.out.println(owner);
 		System.out.println("test znaków diakrytycznych: ZAŻÓŁĆ GĘŚLĄ JAŹŃ");
+		
+		String [][]matrix = new String[7][2];
+		matrix [0][0]="Imię";
+		matrix [1][0]="Nazwisko";
+		matrix [2][0]="Rok urodzenia";
+		matrix [3][0]="Adres e-mail";
+		matrix [4][0]="Numer buta";
+		matrix [5][0]="Kot";
+		matrix [6][0]="Kawa?";
+		
+		Scanner in = new Scanner(System.in);
+		
+		for(int row=0; row < matrix.length; row++){
+			matrix [row][1] = " ";
+			System.out.println("Podaj: " + matrix [row][0]);
+			matrix [row][1] =  in.nextLine();
+		}
+		
+		for(int row=0; row < matrix.length; row++){
+			System.out.println(matrix [row][0] + " " + matrix [row][1]);
 		//ToBeContinued
 	}
+}
 }
