9f20788b60c375e177911d8708aff34576d0974a
Anna Siewierska <absinthic@gmail.com>
2015-04-18 11:25:40 +0200

    extermination

---
+++ src/task_3/Exterminate.java
@@ -0,0 +1,11 @@
+package task_3;
+
+public class Exterminate {
+
+	public static void main(String[] args) {
+		String owner="Andziorek";
+		System.out.println(owner);
+		System.out.println("test znaków diakrytycznych: ZAŻÓŁĆ GĘŚLĄ JAŹŃ");
+		//ToBeContinued
+	}
+}
