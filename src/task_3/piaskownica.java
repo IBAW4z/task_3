3b3a144790a7f14602f56c6a6c046d3fcf8b9fe3
Anna Siewierska <absinthic@gmail.com>
2015-04-20 17:25:28 +0200

    commit

--- src/task_3/piaskownica.java
+++ src/task_3/piaskownica.java
@@ -2,18 +2,14 @@ package task_3;
 
 public class piaskownica {
 	public static void main(String[] args) {
-		System.out.println("ARenc  ASmole� KSk�ra  MPietruszewska TKowalski"); //St�d usuwacie siebie
-		System.out.println(); //Nowa linia spos�b1
-		System.out.print("\n"); //Nowa linia spos�b2
-		System.out.println("Kto: co� tam"); //Ka�dy z Was dopisuje swoj� linijk� pod moj�:
-		System.out.println("ASiewierska: doda�am co� do kodu"); // W zwi�zku z wykrzaczeniem wywali�am grafomani� Pa�, kt�re nawet na instrukcje nie poczeka�y!
-		System.out.println("Finally"); //po co mam co� pisa�? (Ola)...
-		System.out.println("ARenc: Nie uda si�");
-        System.out.println("NAnna: Angels have teh phone box");//kolejna próba
+		System.out.println("ASiewierska: Usuwacie swoje linijki"); 
+		System.out.println("Finally");
+		System.out.println("ARenc: Nie uda się");
+        System.out.println("NAnna: Angels have the phone box");//kolejna próba
         System.out.println("RUkidding me: patrz nick");
         System.out.println("PUszko: plzzz"); //zmiana2
         System.out.println("KStrzebonska: Witam, klaniam sie"); 
-        System.out.println("Metz: wirtuoz merge confilcts te� wita"); 
+        System.out.println("Metz: wirtuoz merge confilcts też wita"); 
         System.out.println("LSzetlak: HelloTeam4");
         System.out.println("TKowalski: yoTeam");
         System.out.println("Magda: czesc");
