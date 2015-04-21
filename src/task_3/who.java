1b74ab05ddaf37b26601345b64a1dd48ef9989f0
Anna <annasmo@student.agh.edu.pl>
2015-04-21 22:27:29 +0200

    whovian class

---
+++ src/task_3/who.java
@@ -0,0 +1,42 @@
+package task_3;
+import java.util.Scanner;
+
+/**
+ * Klasa implementuje wiedzę o DW w środowisku Java ;P
+ * Created by Anna on 2015-04-21.
+ */
+public class who {
+    private final static String TWORCA="NAnna";
+    public String companion;
+    public String doctor;
+
+    public static void showTworca() {
+        System.out.println(TWORCA);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---właściciel---");
+        who.showTworca();
+        System.out.println("----------------");
+        who[] Mates = new who[8];
+        Scanner w=new Scanner(System.in);
+
+        for(int i=0;i<Mates.length;i++){
+            Mates[i] = new who();
+        }
+
+        for(int j=0; j<Mates.length; j++){
+            System.out.println("---Sezon "+(j+1)+"---");
+            System.out.println("Podaj towarzyszkę:");
+            Mates[j].companion=w.nextLine();
+            System.out.println("Podaj Doktora");
+            Mates[j].doctor=w.nextLine();
+        }
+        for(int j=0;j<Mates.length;j++){
+            System.out.println((j+1)+". "+Mates[j].doctor+"+"+Mates[j].companion);
+        }
+
+    }
+
+}
+
