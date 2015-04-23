3e65b9468598f94c8e896daa44b13561696212df
happyme7 <happyme@student.agh.edu.pl>
2015-04-23 17:07:15 +0200

    żebym tylko nic nie popsuła...

--- src/task_3/piaskownica.java
+++ src/task_3/piaskownica.java
@@ -2,8 +2,7 @@ package task_3;
 
 public class piaskownica {
 	public static void main(String[] args) {
-		System.out.println("ASiewierska: Usuwacie swoje linijki"); 
-        System.out.println("KStrzebonska: Witam, klaniam sie"); 
+		System.out.println("ASiewierska: Usuwacie swoje linijki");  
         System.out.println("LSzetlak: HelloTeam4");
         System.out.println("TKowalski: yoTeam");
         System.out.println("Magda: czesc");
